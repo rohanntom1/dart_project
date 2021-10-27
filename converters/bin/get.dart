7e2c38a0c0eec9a2e950d2f3bcdd3cdd381fe4fe
rohanntom <81402683+rohanntom@users.noreply.github.com>
2021-10-27 13:06:14 +0530

    Add files via upload

--- converters/bin/get.dart
+++ converters/bin/get.dart
@@ -1,15 +1,41 @@
+//import 'dart:html';
 import 'dart:io';
 import 'dart:core';
 import 'dart:async';
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-Future<String> getCurrencies() async {
-  http.Response response =
-      await http.get(Uri.parse('https://exchangeratesapi.io/'));
-  return (response.body);
+//import 'rate.dart';
+
+// Future<String> getCurrencies() async {
+//   http.Response response = await http.get(Uri.parse(
+//       'https://v6.exchangerate-api.com/v6/5366b4e648db52bac7661c6f/codes'));
+//   return (response.body);
+// }
+
+Future<double> convertRate(
+    String source, String destination, double amount) async {
+  http.Response response = await http.get(Uri.parse(
+      'https://v6.exchangerate-api.com/v6/5366b4e648db52bac7661c6f/pair/${source.toUpperCase()}/${destination.toUpperCase()}/$amount'));
+  var body = jsonDecode(response.body);
+  final lists = body["conversion_result"];
+  return lists;
 }
 
 void main() async {
-  String currencies = await getCurrencies();
-  print(currencies);
+  // print(await getCurrencies());
+  double amount;
+  stdout.write('\nEnter the source currency: ');
+  String? source = stdin.readLineSync();
+  stdout.write('Enter the destination currency: ');
+  String? destination = stdin.readLineSync();
+  stdout.write('Enter the amount: ');
+  amount = double.parse(stdin.readLineSync()!);
+  if (source == null || destination == null) {
+    throw Exception("Please enter the valid value");
+  }
+  final rates = await convertRate(source, destination, amount);
+  //double rate = rates;
+  print(
+      'The conversion rate of ${source.toUpperCase()} to ${destination.toUpperCase()} is $rates');
 }
--- converters/bin/rate.dart
+++ converters/bin/rate.dart
@@ -1,18 +1,20 @@
 import 'dart:async';
+import 'dart:io';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiClient {
-  final Uri currencyURL = Uri.https("free.currconv.com", "/api/v7/currencies",
-      {"apiKey": "b5d2736b7f9d1ca257fd"});
+  final Uri currencyURL = Uri.parse(
+      'http://api.exchangeratesapi.io/v1/latest?access_key=0bc1b1cafb8a8e7f9b2971c32fad153b');
 
   Future<List<String>> getCurrencies() async {
     http.Response res = await http.get(currencyURL);
     if (res.statusCode == 200) {
       var body = jsonDecode(res.body);
       var list = body["results"];
+
       List<String> currencies = (list.keys).toList();
-      return currencies;
+      return (currencies);
     } else {
       throw Exception("Failed to connect to API");
     }
@@ -21,5 +23,14 @@ class ApiClient {
 
 void main() async {
   ApiClient client = ApiClient();
-  print(client.getCurrencies());
+  print(await client.getCurrencies());
+  double rate, amount;
+  stdout.write('Enter the source currency: ');
+  String? source = stdin.readLineSync();
+  stdout.write('Enter the destination currency: ');
+  String? destination = stdin.readLineSync();
+  stdout.write('Enter the amount: ');
+  amount = double.parse(stdin.readLineSync()!);
+  rate = amount * 2;
+  print('The conversion rate is $rate');
 }
