e08ce082e3a03a0c739a3529b2c419fc7f192ce8
rohanntom <81402683+rohanntom@users.noreply.github.com>
2021-10-28 15:33:48 +0530

    Add files via upload

--- converters/bin/get.dart
+++ converters/bin/get.dart
@@ -1,16 +1,19 @@
-//import 'dart:html';
 import 'dart:io';
 import 'dart:core';
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-//import 'rate.dart';
-
-// Future<String> getCurrencies() async {
+// Future<List> getCurrencies() async {
 //   http.Response response = await http.get(Uri.parse(
 //       'https://v6.exchangerate-api.com/v6/5366b4e648db52bac7661c6f/codes'));
-//   return (response.body);
+//   if (response.statusCode == 200) {
+//     var body = jsonDecode(response.body);
+//     final lists = body["supported_codes"];
+//     return lists;
+//   } else {
+//     throw Exception("Failed to connect to API");
+//   }
 // }
 
 Future<double> convertRate(
@@ -27,19 +30,25 @@ Future<double> convertRate(
 }
 
 void main() async {
-  // print(await getCurrencies());
+  //List currency = await getCurrencies();
   double amount;
-  stdout.write('\nEnter the source currency: ');
-  String? source = stdin.readLineSync();
-  stdout.write('Enter the destination currency: ');
-  String? destination = stdin.readLineSync();
-  stdout.write('Enter the amount: ');
-  amount = double.parse(stdin.readLineSync()!);
-  if (source == null || destination == null) {
-    throw Exception("Please enter a valid value");
+  try {
+    stdout.write('\nEnter the source currency: ');
+    String? source = stdin.readLineSync();
+    if (source == null || source.isEmpty) {
+      throw Exception("Please enter a valid currency");
+    }
+    stdout.write('Enter the destination currency: ');
+    String? destination = stdin.readLineSync();
+    if (destination == null || destination.isEmpty) {
+      throw Exception("Please enter a valid currency");
+    }
+    stdout.write('Enter the amount: ');
+    amount = double.parse(stdin.readLineSync()!);
+    final rates = await convertRate(source, destination, amount);
+    print(
+        'The conversion result of ${source.toUpperCase()} to ${destination.toUpperCase()} is $rates');
+  } catch (e) {
+    print('Exception occurs: $e');
   }
-  final rates = await convertRate(source, destination, amount);
-  //double rate = rates;
-  print(
-      'The conversion rate of ${source.toUpperCase()} to ${destination.toUpperCase()} is $rates');
 }
